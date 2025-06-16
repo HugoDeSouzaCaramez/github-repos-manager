eff12039695e694b467e274dea0a2e0fc8bab2ca
Hugo de Souza Caramez <hugo.7.musica@gmail.com>
2025-06-16 14:48:53 -0300

    refact: retirando código morto

--- backend/src/repos/repos.controller.ts
+++ backend/src/repos/repos.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Query } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
 import { Repo } from './repo.entity';
 import { Repository } from 'typeorm';
 import { InjectRepository } from '@nestjs/typeorm';
